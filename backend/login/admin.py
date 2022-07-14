4d91bde7a541031cf686a3bf2ea4a9b73fd1d595
Szabó Balázs <szabo.balazs2017@gmail.com>
2022-07-14 21:07:43 +0300

    Create User and UserAdmin

--- backend/login/admin.py
+++ backend/login/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import display
+
+from login.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'get_name', 'email', 'birthday')
+
+    @display(description='fullname')
+    def get_name(self, obj):
+        return obj.firstname + " " + obj.lastname
+
+
+admin.site.register(User, UserAdmin)
--- backend/login/models.py
+++ backend/login/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
 
 
 class User(models.Model):
 
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=30)
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
-    age = models.IntegerField(default=0)
+    birthday = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.username
