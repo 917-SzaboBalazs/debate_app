8114ca9c32f0857049761550fe1771d5f1977f55
Hunor Acs <hunii197@icloud.com>
2022-07-19 15:48:13 +0300

    Added about-us page

---
+++ frontend/src/pages/AboutUs/About-Us.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Us from '../../components/Us/Us';
+import './About-Us.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us--container">
+        <Us />
+    </div>
+  )
+}
+
+export default AboutUs
