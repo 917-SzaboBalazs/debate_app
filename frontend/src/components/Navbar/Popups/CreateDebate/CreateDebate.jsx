d8d430b42b4bd4649a9767eeafc69f89e7c1e927
Hunor Acs <hunii197@icloud.com>
2022-07-22 21:43:56 +0300

    Added Ceate-Debate/select-debate-types
    Kiválasztható, hogy milyen vitát szeretnél indítani, majd a Create gomb kiírja ezt a képernyőre.

--- frontend/src/components/Navbar/Popups/CreateDebate/CreateDebate.jsx
+++ frontend/src/components/Navbar/Popups/CreateDebate/CreateDebate.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import Select from 'react-select';
 import { useState } from 'react';
 
 import './CreateDebate.css'
+import { logDOM } from '@testing-library/react';
 
 function CreateDebate(props) {
+  const [ debateType, setType ] = useState('britt-parlamenti');
+
+  const options = [
+    { value: 'britt-parlamenti', label: 'Britt Parlamenti' },
+    { value: 'lincoln-douglas', label: 'Lincoln-Douglas' },
+    { value: 'spontan', label: 'Spontán' },
+    { value: 'publikus', label: 'Publikus' }
+  ];
+
   return (props.trigger) ? (
     <div className="create-debate">
         <div className="create-debate--inner">
             <button className="close-btn" onClick={() => props.setTrigger(false)}>X</button>
             <h3 className="create-debate--text">
-                Let's Create a Debate!
+                Select debate type
             </h3>
-            <p>Ide teszunk be minden cuccot.</p>
-            <button className="create" >
+            <Select 
+              className="select-debate" 
+              defaultValue={options[0]}
+              options={options} 
+              onChange={(e) => setType(e.value)}
+              />
+            <button className="create" onClick={() => console.log(debateType)}>
                 Create!
             </button>
         </div>
