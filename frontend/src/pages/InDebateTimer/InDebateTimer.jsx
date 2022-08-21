f24f6b6e8c92fea1453bf34bf39418738e838c75
Hunor Acs <hunii197@icloud.com>
2022-08-21 14:16:42 +0300

    indebate back + next btn

--- frontend/src/pages/InDebateTimer/InDebateTimer.jsx
+++ frontend/src/pages/InDebateTimer/InDebateTimer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+// icon imports 
 import GreenStart from '../../images/start-green.svg';
 import WhiteStart from '../../images/start-white.svg';
 import RedStop from '../../images/stop-red.svg';
 import WhiteStop from '../../images/stop-white.svg';
 import BlueRes from '../../images/reset-blue.svg';
 import WhiteRes from '../../images/reset-white.svg';
+import YellowNext from '../../images/next-yellow.svg';
+import WhiteNext from '../../images/next-white.svg';
 
 import './InDebateTimer.css'
 
@@ -15,6 +18,11 @@ function InDebateTimer() {
     const [ running, setRunning ] = useState(false);
     const [ isMouse, setMouse ] = useState(false);
     const [ isMouseRes, setMouseRes ] = useState(false);
+    const [ isMouseNext, setMouseNext ] = useState(false);
+    const [ isMouseBack, setMouseBack ] = useState(false);
+    const [ currentlySpeaking, setCurrentlySpeaking ] = useState(1);
+
+    const speakerRole = [ 'PRO-Start', 'CON-Start', 'PRO-Start', 'CON-Start', 'PRO-End', 'CON-End', 'PRO-End', 'CON-end'];
 
     useEffect(() => {
         let interval;
@@ -44,9 +52,22 @@ function InDebateTimer() {
         setMouseRes(prev => !prev);
     }
 
+    const handleMouseNext = () => {
+        setMouseNext(prev => !prev);
+    }
+
+    const handleMouseBack = () => {
+        setMouseBack(prev => !prev);
+    }
+
     return (
         <div className="indebate--base base">
             <div className="indebate--container container bg-light">
+                <div className="row indebate--current-speaker">
+                    <div className="col-12 text-center">
+                        <h3>Current speaker: {currentlySpeaking} ({speakerRole[currentlySpeaking - 1]})</h3>
+                    </div>
+                </div>
                 <div className="indebate--stopwatch row text-center">
                     <div className="indebate--numbers">
                         <span>{("0" + Math.floor((seconds / 60000) % 60)).slice(-2)}:</span>
@@ -71,7 +92,10 @@ function InDebateTimer() {
                         <br/>
                         <button 
                             className='indebate--button col-12 indebate--reset-button'
-                            onClick={() => setSeconds(0)}
+                            onClick={() => {
+                                setSeconds(0);
+                                setRunning(false)
+                                }}
                             onMouseOver={handleMouseRes}
                             onMouseOut={handleMouseRes}
                         >  
@@ -83,6 +107,41 @@ function InDebateTimer() {
                             className='indebate--reset'
                             />
                         </button>       
+                        <br/>
+                        <button 
+                            className="indebate--button indebate--back-button col-12"
+                            onClick={() => {
+                                if ( currentlySpeaking > 1 )
+                                setCurrentlySpeaking(prev => prev - 1);
+                                }}
+                            onMouseOver={handleMouseBack}
+                            onMouseOut={handleMouseBack}
+                        >
+                            <img src={
+                                !isMouseBack ? 
+                                    WhiteNext :
+                                    YellowNext
+                            } 
+                            className='indebate--back'
+                            />
+                        </button>
+                        <button 
+                            className="indebate--button indebate--next-button col-12 text-center"
+                            onClick={() => {
+                                if ( currentlySpeaking < 8 )
+                                setCurrentlySpeaking(prev => prev + 1);
+                                }}
+                            onMouseOver={handleMouseNext}
+                            onMouseOut={handleMouseNext}
+                        >
+                            <img src={
+                                !isMouseNext ? 
+                                    WhiteNext :
+                                    YellowNext
+                            } 
+                            className='indebate--next'
+                            />
+                        </button>
                     </div>
                 </div>
             </div>
