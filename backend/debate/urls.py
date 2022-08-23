e328a6a3215c1f058e52b7b43e712565fea6c1cc
Szabó Balázs <szabo.balazs2017@gmail.com>
2022-08-23 18:50:02 +0300

    Create entry_code

--- backend/debate/urls.py
+++ backend/debate/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from debate.views import CurrentDebateView, CreateDebateView
+from debate.views import CurrentDebateView, CreateDebateView, DebateCodeView
 
 app_name = "debate"
 
 urlpatterns = [
     path('', CreateDebateView.as_view(), name='create_debate'),
     path('current/', CurrentDebateView.as_view(), name='current_debate'),
+    path('entry-code/', DebateCodeView.as_view(), name='debate_code'),
 ]
--- backend/debate/views.py
+++ backend/debate/views.py
@@ -1,8 +1,12 @@
+import random
+import string
+
 from django.http import Http404
 from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from debate.models import Debate
 from debate.serializers import DebateSerializer
@@ -27,3 +31,11 @@ class CreateDebateView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Debate.objects.all()
     serializer_class = DebateSerializer
+
+
+class DebateCodeView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        random_string = ''.join(random.choices(string.ascii_lowercase, k=8))
+        return Response(data=random_string)
