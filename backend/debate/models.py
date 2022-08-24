4865bc1b3b2e484996be453532c72d657df0ec50
Szabó Balázs <szabo.balazs2017@gmail.com>
2022-08-24 16:01:59 +0300

    Create unique entry codes

--- backend/debate/models.py
+++ backend/debate/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Debate(models.Model):
     type = models.CharField(max_length=50)
-    entry_code = models.CharField(max_length=8)
+    entry_code = models.CharField(max_length=8, unique=True)
     winner = models.CharField(max_length=50, default="no winner")
     status = models.CharField(max_length=50, default="lobby")
     date_time = models.DateTimeField(default=timezone.now)
