a48bec24363899109a0fa4743ba29829f196582a
Szabó Balázs <szabo.balazs2017@gmail.com>
2022-09-01 11:40:49 +0300

    Update FinishDebate.jsx

--- frontend/src/components/Popups/FinishDebate/FinishDebate.jsx
+++ frontend/src/components/Popups/FinishDebate/FinishDebate.jsx
@@ -61,10 +61,10 @@ function FinishDebate(props) {
               <h3 className="finish-debate--winner-text col-12 white-text pb-4">Ki nyert?</h3>
               <div className="finish-debate--buttons-field d-flex align-items-center justify-content-center text-center"></div>
               <div className="finish-debate--buttons-container">
-                <button className="finish-debate--pro finish-debate--button col-2" onClick={handlePro}>PRO</button>
-                <button className="finish-debate--con finish-debate--button col-2" onClick={handleCon}>KONTRA</button>
-                <button className="finish-debate--draw finish-debate--button col-2" onClick={handleDraw}>DÖNTETLEN</button>
-                <button className="finish-debate--no-winner finish-debate--button col-3" onClick={handleNoWinner}>NINCS GYŐZTES</button>
+                <button className="finish-debate--pro finish-debate--button col-4" onClick={handlePro}>PRO</button>
+                <button className="finish-debate--con finish-debate--button col-4" onClick={handleCon}>KONTRA</button>
+                <button className="finish-debate--draw finish-debate--button col-4" onClick={handleDraw}>DÖNTETLEN</button>
+                <button className="finish-debate--no-winner finish-debate--button col-4" onClick={handleNoWinner}>NINCS GYŐZTES</button>
               </div>
             </div>
         </div>
