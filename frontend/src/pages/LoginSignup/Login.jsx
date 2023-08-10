101ae00ff94df336eb6dc1b9a90131ad58d62a0e
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-08-10 20:14:00 +0300

    RD fix

--- frontend/src/pages/LoginSignup/Login.jsx
+++ frontend/src/pages/LoginSignup/Login.jsx
@@ -50,14 +50,6 @@ function Login() {
         })
     }
 
-    document.addEventListener('keydown', function(event) {
-      console.log("nyomva")
-      if (event.keyCode === 13) {
-        console.log("enter")
-        handleSubmit();
-      }
-    });
-
     const images = [
       image1,   
       image2,
