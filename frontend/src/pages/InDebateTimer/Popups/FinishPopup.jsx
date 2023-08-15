c3be102de672d794a0858e6d3a505c72f6f0776b
Hunor Acs <hunii197@gmail.com>
2023-08-15 17:46:43 +0300

    #43 finish debate popup

---
+++ frontend/src/pages/InDebateTimer/Popups/FinishPopup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './FinishPopup.css';
+
+export default function FinishPopup(props) {
+  return (props.trigger) ? (
+    <div className="finish-popup">
+        <div className="finish-popup--inner">
+            <p>Do you wish to finish the debate now?</p>
+            <div className="finish-popup-buttons">
+              <button onClick={() => props.setTrigger(false)}>No</button>
+              <button onClick={() => {
+                props.setTrigger(false);
+                props.navigate('/results');
+              }}>Yes</button>
+            </div>
+        </div>
+    </div>
+  ) : '';
+}
