08f43a7234d5e459259cefbc53c61e45218d9898
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-08-20 17:14:57 +0300

    backend#14
    #14: Deepl translate the motion when change the language

--- backend/motion/urls.py
+++ backend/motion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from motion.views import ListMotionsView, RandomMotionView
+from motion.views import ListMotionsView, RandomMotionView, MotionDeeplTranslateView
 
 app_name = "motion"
 urlpatterns = [
     path('', ListMotionsView.as_view(), name="list_motions"),
     path('random/', RandomMotionView.as_view(), name="random_motion"),
+    path('translate/', MotionDeeplTranslateView.as_view(), name="motion_deepl_translate"),
 ]
